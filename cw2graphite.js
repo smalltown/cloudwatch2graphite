fd8b4f78dbd0f20d9bf6f40f7c5c072a6e42922a
root <root@graphite-berkshelf.(none)>
2014-11-20 06:01:48 +0000

    extract all parameter to config

--- cw2graphite.js
+++ cw2graphite.js
@@ -1,110 +1,15 @@
-require('datejs');
+// load metric and credentials configuration
+var conf = require('nconf');
 
-var dateFormat = require('dateformat');
+conf.use('file', { file: './config/credentials.json' });
+credentials = conf.stores.file.store;
 
-var global_options = require('./lib/options.js').readCmdOptions();
+conf.file('./config/metric.json');
+metricConf = conf.stores.file.store;
 
-var cloudwatch = require('aws2js').load('cloudwatch', global_options.credentials.accessKeyId, global_options.credentials.secretAccessKey);
+// set cloudwatch connection 
+var cloudwatch = require('aws2js').load('cloudwatch', credentials.accessKeyId, credentials.secretAccessKey);
+cloudwatch.setRegion(metricConf.region);
 
-cloudwatch.setRegion(global_options.region_name);
-
-var metrics = global_options.metrics_config.metrics
-
-for(index in metrics) {
-	getOneStat(metrics[index], global_options.region_name);
-	//console.log("Session: %j \n", metrics[index]);
-}
-
-function getOneStat(metric,regionName) {
-	var interval = 1440;
-
-	var now = new Date();
-	var then = (interval).minutes().ago()
-
-	if ( metric.Namespace.match(/Billing/) ) {
-	    then.setHours(then.getHours() - 30)
-	}
-
-	var end_time = dateFormat(now, "isoUtcDateTime");
-	var start_time = dateFormat(then, "isoUtcDateTime");
-
-
-	var options = {
-		Namespace: metric.Namespace,
-		MetricName: metric.MetricName,
-		Period: '60',
-		StartTime: start_time,
-		EndTime: end_time,
-		"Statistics.member.1": metric["Statistics.member.1"],
-		Unit: metric.Unit,
-	}
-
-	if ( metric.Namespace.match(/Billing/) ) {
-	    options["Period"] = '28800'
-	}
-
-	metric.name = (global_options.metrics_config.carbonNameSpacePrefix != undefined) ? global_options.metrics_config.carbonNameSpacePrefix + "." : "";
-	metric.name = metric.name.replace("{regionName}",regionName);
-	
-	metric.name += metric.Namespace.replace("/", ".");
-
-	for (var i=1;i<=10;i++) {
-		if (metric["Dimensions.member."+i+".Name"]!==undefined && metric["Dimensions.member."+i+".Value"]!==undefined) {
-			options["Dimensions.member."+i+".Name"] = metric["Dimensions.member."+i+".Name"]
-			options["Dimensions.member."+i+".Value"] = metric["Dimensions.member."+i+".Value"]
-
-			metric.name += "." + metric["Dimensions.member."+i+".Value"];
-		}
-	}
-	
-	metric.name += "." + metric.MetricName;
-	metric.name += "." + metric["Statistics.member.1"];
-	metric.name += "." + metric.Unit;
-
-	metric.name = metric.name.toLowerCase()
-
-	// console.log(metric);
-	cloudwatch.request('GetMetricStatistics', options, function(error, response) {
-		if(error) {
-			console.error("ERROR ! ",error);
-			return;
-		}
-		if (! response.GetMetricStatisticsResult) {
-			console.error("ERROR ! response.GetMetricStatisticsResult is undefined for metric " + metric.name);
-			return;
-		}
-		if (!response.GetMetricStatisticsResult.Datapoints) {
-			console.error("ERROR ! response.GetMetricStatisticsResult.Datapoints is undefined for metric " + metric.name);
-			return;
-		}
-			
-		var memberObject = response.GetMetricStatisticsResult.Datapoints.member;
-		if (memberObject == undefined) {
-			console.error("WARNING ! no data point available for metric " + metric.name);
-			return;
-		}
-
-                //console.log("Session: %j", memberObject);
-
-		var dataPoints;
-		if(memberObject.length === undefined) {
-			dataPoints = [memberObject];
-		} else {
-			// samples might not be sorted in chronological orderi
-			dataPoints = memberObject.sort(function(m1,m2){
-				var d1 = new Date(m1.Timestamp), d2 = new Date(m2.Timestamp);
-				return d1 - d2
-			});
-		}
-		// Very often in Cloudwtch the last aggregated point is inaccurate and might be updated 1 or 2 minutes later
-		// this is not a problem if we choose to overwrite it into graphite, so we read the 3 last points.
-
-		if (dataPoints.length > global_options.metrics_config.numberOfOverlappingPoints) {
-			dataPoints = dataPoints.slice(0, dataPoints.length);
-		}
-		for (var point in dataPoints) {
-			console.log("%s %s %s", metric.name, dataPoints[point][metric["Statistics.member.1"]], parseInt(new Date(dataPoints[point].Timestamp).getTime() / 1000.0));
-		}
-	});
-}
+require('./lib/metric.js').getMetrics(cloudwatch, metricConf);
 
